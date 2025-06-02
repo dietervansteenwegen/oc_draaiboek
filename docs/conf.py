f3460985e8de45312ef784c838ca554b1457aa8f
Dieter Vansteenwegen <github@vansteenwegen.org>
2025-06-02 09:36:51 +0200

    docs: Config for readthedox index page

--- docs/conf.py
+++ docs/conf.py
@@ -43,7 +43,7 @@ templates_path = ["_templates"]
 source_suffix = ".rst"
 
 # The master toctree document.
-master_doc = "00_Inhoudstafel"
+master_doc = "index"
 
 # General information about the project.
 project = "Draaiboek OC De Oogappel"
